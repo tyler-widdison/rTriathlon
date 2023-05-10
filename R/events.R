28108da450bfa24ee10ba1af28b8b7f78e93a3ed
tyler-widdison <tylerperrywiddison@gmail.com>
2023-05-09 21:23:11 -0600

    Push for events

---
+++ R/events.R
@@ -0,0 +1,45 @@
+#' Get triathlon data
+#'
+#' @references \url{https://developers.triathlon.org/reference}
+#' @param startdate date: start date to search for events
+#' @param enddate date: end date to search for events
+#'
+#' @return A data.frame
+#'
+#' @examples
+#' \dontrun{
+#'   tri_get_event_list('2023-01-01', '2023-02-01')
+#' }
+#'
+#' @export
+
+
+tri_get_event_list <- function(startdate, enddate){
+  # function to get events based on filter parameters #
+  url = "https://api.triathlon.org/v1/search/events"
+  npage <- 1
+  bfirst <- TRUE
+  while (npage > 0) {
+    # Query the API
+    res <- GET(url, add_headers(c("apikey" = "2649776ef9ece4c391003b521cbfce7a")),
+               query = list(page = npage, per_page = 100, start_date = startdate, end_date = enddate))
+    # Turn the result in a dataframe
+    df <- jsonlite::fromJSON(rawToChar(res$content))
+    events <- df$data
+    if (length(df$data) > 0 ) {
+      npage <- npage + 1
+      events <- events %>% dplyr::filter(event_cancelled == FALSE) %>% select(-`_highlightResult`)
+      #events <- events[,c("event_id","event_title","event_venue","event_country","event_finish_date")]
+      if (bfirst == TRUE) {
+        dfout <- events
+        bfirst <- FALSE
+      } else {
+        dfout <- rbind(dfout,events)
+      }
+    } else{
+      npage <- 0
+    }
+  }
+  return(dfout)
+}
+
