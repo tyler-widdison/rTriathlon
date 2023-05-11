a58f5625160fcd52bcc59f9bd288d25d876cb9d8
tyler-widdison <tylerperrywiddison@gmail.com>
2023-05-11 17:23:06 -0600

    Try catch commmit

--- R/program_listings.R
+++ R/program_listings.R
@@ -13,12 +13,17 @@
 #' @export
 
 tri_program_list_get <- function(no) {
-  do.call(rbind, lapply(no, function(event_id) {
-    url <- paste0("https://api.triathlon.org/v1/events/", event_id, "/programs")
-    res <- httr::GET(url, httr::add_headers(c("apikey" = "2649776ef9ece4c391003b521cbfce7a")))
-    df <- jsonlite::fromJSON(rawToChar(res$content))
-    prog <- df$data
-    prog <- prog %>% dplyr::filter(results == TRUE)
-    return(prog)
+  tryCatch({
+    do.call(rbind, lapply(no, function(event_id) {
+      url <- paste0("https://api.triathlon.org/v1/events/", event_id, "/programs")
+      res <- httr::GET(url, httr::add_headers(c("apikey" = "2649776ef9ece4c391003b521cbfce7a")))
+      df <- jsonlite::fromJSON(rawToChar(res$content))
+      prog <- df$data
+      prog <- prog %>% dplyr::filter(results == TRUE)
+      return(prog)
     }))
-  }
+  }, error = function(e) {
+    message("An error occurred: ", e$message)
+    return(NULL)
+  })
+}
